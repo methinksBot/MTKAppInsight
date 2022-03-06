61caf789c03c960a5e5d0a256ac7a8c9a3530356
Philip Yun <philip@methinks.io>
2022-03-06 09:56:01 -0800

    0.7.3 release

---
+++ methinksSDK-iOS.xcframework/ios-arm64_armv7/methinksSDK-iOS.framework/Headers/methinksiOS.h
@@ -0,0 +1,13 @@
+//
+//  methinksiOS.h
+//  methinksiOS
+//
+//  Created by Philip Yun on 7/23/20.
+//  Copyright © 2020 Philip Yun. All rights reserved.
+//
+
+#ifndef methinksiOS_h
+#define methinksiOS_h
+#import "MTKSDK.h"
+//#import <methinksiOS/MTKSDK.h>
+#endif /* methinksiOS_h */
