435212ebda4e4a55862dc75de7655197dc644bdc
Philip Yun <philip@methinks.io>
2022-03-06 16:34:32 -0800

    0.7.4 release

---
+++ MTKAppInsight.xcframework/ios-arm64_armv7/MTKAppInsight.framework/Headers/MTKAppInsight.h
@@ -0,0 +1,13 @@
+//
+//  methinksiOS.h
+//  methinksiOS
+//
+//  Created by Philip Yun on 7/23/20.
+//  Copyright © 2020 Philip Yun. All rights reserved.
+//
+
+#ifndef MTKAppInsight_h
+#define MTKAppInsight_h
+#import "MTKSDK.h"
+//#import <methinksiOS/MTKSDK.h>
+#endif /* methinksiOS_h */
